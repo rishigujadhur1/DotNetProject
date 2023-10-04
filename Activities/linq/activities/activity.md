1b580cd8d24b3282e81acfb4a0ae03cd15770371
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-05 00:36:17 +0400

    LINQ questions and answers

--- Activities/linq/activities/activity.md
+++ Activities/linq/activities/activity.md
@@ -1,1 +1,45 @@
 1. Write a function that takes a list of strings and returns a new list containing only the strings that start with the letter "A."
+
+Filtering with LINQ:
+
+2. Write a LINQ query to filter a list of strings, selecting only those that contain the word "apple."
+
+Projection with LINQ:
+
+3. Given a list of Person objects (with properties Name and Age), write a LINQ query to project only the names of people who are older than 30.
+
+Sorting with LINQ:
+
+4. Create a LINQ query to sort a list of integers in ascending order.
+
+Grouping with LINQ:
+
+5. Given a list of Student objects (with properties Name and Grade), use LINQ to group students by their grade and calculate the average grade for each group.
+
+Joining with LINQ:
+
+6. Given two lists of Order and Customer objects (with properties CustomerId in Order and Id in Customer), write a LINQ query to join the lists and select orders with customer information, including the customer's name.
+
+Aggregate Functions with LINQ:
+
+7. Calculate the sum of all the even numbers in a list of integers using LINQ.
+
+Distinct with LINQ:
+
+8. Write a LINQ query to retrieve distinct elements from a list of strings, ignoring case sensitivity.
+
+Partitioning with LINQ:
+
+9. Create a LINQ query to skip the first 3 elements and take the next 5 elements from a list of integers.
+
+Nested Queries with LINQ:
+
+10. Given a list of Department objects (with properties Id and Name) and a list of Employee objects (with properties Name and DepartmentId), write a LINQ query to find the department with the most employees.
+
+Filtering with Multiple Conditions:
+
+11. Filter a list of Product objects (with properties Name, Category, and Price) to select products that belong to the "Electronics" category and have a price less than $500.
+
+Grouping and Aggregating with LINQ:
+
+12. Given a list of Order objects (with properties OrderId, CustomerId, and TotalAmount), write a LINQ query to group orders by customer and calculate the total amount spent by each customer.
--- Activities/linq/activities/answers.cs
+++ Activities/linq/activities/answers.cs
@@ -2,3 +2,111 @@ public List<string> FilterStringsStartingWithA(List<string> inputList)
 {
     return inputList.Where(s => s.StartsWith("A")).ToList();
 }
+
+// Classes used for the next LINQ tasks
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Grade { get; set; }
+}
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public int CustomerId { get; set; }
+    public decimal TotalAmount { get; set; }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int DepartmentId { get; set; }
+}
+
+//2 Filtering with LINQ:
+var filteredList = originalList.Where(item => item.Contains("apple")).ToList();
+
+//3 Projection with LINQ:
+var projectedNames = persons.Where(p => p.Age > 30).Select(p => p.Name).ToList();
+
+//4 Sorting with LINQ:
+var sortedList = numbers.OrderBy(n => n).ToList();
+
+//5 Grouping with LINQ:
+var groupedResults = students.GroupBy(s => s.Grade)
+                             .Select(g => new
+                             {
+                                 Grade = g.Key,
+                                 AverageGrade = g.Average(s => s.Grade)
+                             })
+                             .ToList();
+
+//6 Joining with LINQ:
+var query = from order in orders
+            join customer in customers on order.CustomerId equals customer.Id
+            select new
+            {
+                OrderId = order.OrderId,
+                CustomerName = customer.Name
+            };
+
+var result = query.ToList();
+
+//7 Aggregate Functions with LINQ:
+var sumOfEvens = numbers.Where(n => n % 2 == 0).Sum();
+
+//8 Distinct with LINQ:
+var distinctStrings = stringList.Distinct(StringComparer.OrdinalIgnoreCase).ToList();
+
+//9 Partitioning with LINQ:
+var result = numbers.Skip(3).Take(5).ToList();
+
+//10 Nested Queries with LINQ:
+var departmentWithMostEmployees = departments
+    .Select(d => new
+    {
+        Department = d,
+        EmployeeCount = employees.Count(e => e.DepartmentId == d.Id)
+    })
+    .OrderByDescending(dept => dept.EmployeeCount)
+    .FirstOrDefault();
+
+// Access the department with the most employees: departmentWithMostEmployees.Department
+
+//11 Filtering with Multiple Conditions:
+var filteredProducts = products.Where(p => p.Category == "Electronics" && p.Price < 500).ToList();
+
+//12 Grouping and Aggregating with LINQ:
+var customerTotalSpent = orders
+    .GroupBy(o => o.CustomerId)
+    .Select(group => new
+    {
+        CustomerId = group.Key,
+        TotalAmountSpent = group.Sum(o => o.TotalAmount)
+    })
+    .ToList();
