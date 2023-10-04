7f2099e86229cfd2c5eb1a6062ede93640d547c3
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-04 23:48:45 +0400

    questions and answers for methods

--- Activities/methods/activities/activity.md
+++ Activities/methods/activities/activity.md
@@ -1,1 +1,37 @@
 1. Define a function called CalculateFactorial that calculates and returns the factorial of a positive integer parameter.
+
+Method Overloading:
+
+2. Write a class called Calculator with overloaded methods for addition, subtraction, multiplication, and division. These methods should take either two integers or two doubles as parameters and return the result.
+
+Method Overriding:
+
+3. Create a base class called Shape with a virtual method CalculateArea. Then, create two derived classes Circle and Rectangle that override the CalculateArea method to calculate the area of a circle and a rectangle, respectively.
+
+Static Methods:
+
+4. Design a class MathUtility with a static method CalculateAverage that calculates the average of an array of integers. Demonstrate how to use this method.
+
+Extension Methods:
+
+5. Create an extension method for the string class that counts the number of words in a given sentence. Use this extension method to count the words in a string.
+
+Optional Parameters:
+
+6. Implement a method called Greet that takes a person's name as a parameter and an optional greeting message. If the greeting message is not provided, use a default message like "Hello." Display the greeting.
+
+Named Parameters:
+
+7. Modify the Greet method from the previous task to accept named parameters for both the name and greeting message.
+
+Async Methods:
+
+8. Create an asynchronous method that simulates fetching data from a web API with a delay of 2 seconds. Use the async and await keywords to ensure non-blocking execution.
+
+Lambda Expressions:
+
+9. Create a method that uses a lambda expression to filter a list of integers and returns a new list containing only even numbers.
+
+Delegate as a Method Parameter:
+
+10. Design a method that accepts a delegate as a parameter and uses it to perform a specific operation on a list of integers. Pass different delegate functions to test different operations (e.g., sum, product, average).
--- Activities/methods/activities/answers.cs
+++ Activities/methods/activities/answers.cs
@@ -1,3 +1,5 @@
+//1
+
 public int CalculateFactorial(int n)
 {
     if (n == 0)
@@ -9,3 +11,149 @@ public int CalculateFactorial(int n)
         return n * CalculateFactorial(n - 1);
     }
 }
+
+//2 Method Overloading:
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    // Implement similar methods for subtraction, multiplication, and division.
+}
+
+//3 Method Overriding:
+public class Shape
+{
+    public virtual double CalculateArea()
+    {
+        return 0;
+    }
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
+
+public class Rectangle : Shape
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+
+    public override double CalculateArea()
+    {
+        return Length * Width;
+    }
+}
+
+//4 Static Methods:
+public class MathUtility
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers.Length == 0)
+            return 0;
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        return (double)sum / numbers.Length;
+    }
+}
+
+//5 Extension Methods:
+public static class StringExtensions
+{
+    public static int WordCount(this string input)
+    {
+        // Split the input string into words and count them.
+        return input.Split(new[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+}
+
+// Usage:
+string sentence = "This is a sample sentence.";
+int wordCount = sentence.WordCount();
+
+//6 Optional Parameters:
+public void Greet(string name, string greeting = "Hello")
+{
+    Console.WriteLine($"{greeting}, {name}!");
+}
+
+//7 Named Parameters:
+public void Greet(string name, string greeting = "Hello")
+{
+    Console.WriteLine($"{greeting}, {name}!");
+}
+
+// Usage:
+Greet(name: "Alice", greeting: "Hi");
+
+// 8 Async Methods:
+public async Task GetDataAsync()
+{
+    await Task.Delay(2000); // Simulate a delay of 2 seconds.
+    Console.WriteLine("Data fetched asynchronously.");
+}
+
+// Usage:
+await GetDataAsync();
+
+// 9 Lambda Expressions:
+public List<int> FilterEvenNumbers(List<int> numbers)
+{
+    return numbers.Where(num => num % 2 == 0).ToList();
+}
+
+//10 Delegate as a Method Parameter:
+public delegate double MathOperation(List<int> numbers);
+
+public double Sum(List<int> numbers)
+{
+    return numbers.Sum();
+}
+
+public double Product(List<int> numbers)
+{
+    double result = 1;
+    foreach (int num in numbers)
+    {
+        result *= num;
+    }
+    return result;
+}
+
+public double Average(List<int> numbers)
+{
+    return numbers.Average();
+}
+
+// Usage:
+MathOperation operation = Sum;
+double result = operation(numbers);
