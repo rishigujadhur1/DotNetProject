e10a5813a22da168ecda85a1ec0fe2288efea857
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-05 14:37:19 +0400

    loop questions

---
+++ Activities/loops/questions/questions.cs
@@ -0,0 +1,63 @@
+// For Loop Output:
+// Q1. What is the output of the following C# code?
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+}
+
+// Options:
+// a) 0 1 2 3 4
+// b) 1 2 3 4 5
+// c) 4 3 2 1 0
+// d) 5 5 5 5 5
+
+// While Loop:
+// 2. While Loop Output:
+// Q2. What is the output of the following C# code?
+
+int x = 3;
+while (x > 0)
+{
+    Console.Write(x + " ");
+    x--;
+}
+
+// Options:
+
+// a) 3 2 1
+// b) 3 2
+// c) 3 3 3
+// d) 1 2 3
+
+// Foreach Loop:
+// 3. Foreach Loop Output:
+// Q3. Given the following C# code, what will be the output?
+
+string[] fruits = { "Apple", "Banana", "Orange", "Mango" };
+foreach (string fruit in fruits)
+{
+    Console.Write(fruit + " ");
+}
+
+// Options:
+
+// a) Apple Banana Orange Mango
+// b) Mango Orange Banana Apple
+// c) AppleMangoBananaOrange
+// d) Apple, Banana, Orange, Mango
+
+// Do-While Loop Output:
+// Q4. What is the output of the following C# code?
+int num = 5;
+do
+{
+    Console.Write(num + " ");
+    num--;
+} while (num > 0);
+
+// Options:
+
+// a) 5 4 3 2 1
+// b) 1 2 3 4 5
+// c) 0 1 2 3 4
+// d) 5 5 5 5 5
