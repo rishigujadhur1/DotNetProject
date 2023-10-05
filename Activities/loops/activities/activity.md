5f7b4da905c8bc11ae982b7ef10fa75c439f7718
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-05 14:59:25 +0400

    Loop activities

--- Activities/loops/activities/activity.md
+++ Activities/loops/activities/activity.md
@@ -1,1 +1,30 @@
-1. Create a function named PrintNumbers that takes an integer parameter n and prints all numbers from 1 to n using a loop.
+0. Create a function named PrintNumbers that takes an integer parameter n and prints all numbers from 1 to n using a loop.
+
+For Loop Activities:
+
+1. Calculate Factorial:
+   Write a function called CalculateFactorial that takes an integer parameter n and calculates and returns the factorial of n. Use a for loop to perform the calculation.
+
+Print Multiplication Table: 
+2. Write a program that takes an integer input from the user and uses a for loop to print the multiplication table for that number from 1 to 10.
+
+While Loop Activities: 
+3. Guess the Number:
+Write a program that generates a random number between 1 and 100 and asks the user to guess the number. Use a while loop to keep asking for guesses until the user correctly guesses the number.
+
+Sum of Digits:
+ 4. Write a function called SumOfDigits that takes an integer parameter and calculates and returns the sum of its digits using a while loop.
+
+Foreach Loop Activities:
+ 5. Find Maximum Value:
+Write a function called FindMaxValue that takes an array of integers as input and uses a foreach loop to find and return the maximum value in the array.
+
+Average of Grades:
+ 6. Write a program that prompts the user to enter a list of student grades. Use a foreach loop to calculate and display the average grade.
+Do-While Loop Activities: 7. User Menu with
+
+Do-While: 
+7. Write a program that displays a menu to the user with options (e.g., "1. Add," "2. Subtract," "3. Quit"). Use a do-while loop to keep displaying the menu until the user chooses to quit.
+
+Factorial with Do-While: 
+8. Write a function called CalculateFactorialWithDoWhile that takes an integer parameter n and calculates and returns the factorial of n using a do-while loop.
--- Activities/loops/activities/answer.cs
+++ Activities/loops/activities/answer.cs
@@ -1,7 +1,149 @@
+// 0
 public void PrintNumbers(int n)
 {
     for (int i = 1; i <= n; i++)
     {
         Console.WriteLine(i);
     }
+}
+
+// // 1
+// Calculate Factorial:
+// You can calculate the factorial of a number n using a for loop as follows:
+public static long CalculateFactorial(int n)
+{
+    long factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+// // 2 Print Multiplication Table:
+// Here's a program that prints the multiplication table for a given number using a for loop:
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{number} x {i} = {number * i}");
+}
+
+// 3. While Loop Activities:
+
+// Guess the Number:
+// Here's a program that lets the user guess a random number using a while loop:
+
+Random random = new Random();
+int targetNumber = random.Next(1, 101);
+int attempts = 0;
+
+while (true)
+{
+    Console.Write("Guess the number: ");
+    int guess = int.Parse(Console.ReadLine());
+    attempts++;
+
+    if (guess == targetNumber)
+    {
+        Console.WriteLine($"Congratulations! You guessed it in {attempts} attempts.");
+        break;
+    }
+    else if (guess < targetNumber)
+    {
+        Console.WriteLine("Try a higher number.");
+    }
+    else
+    {
+        Console.WriteLine("Try a lower number.");
+    }
+}
+
+// 4. Sum of Digits:
+// You can calculate the sum of the digits of an integer using a while loop as follows
+public static int SumOfDigits(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+//5. Foreach Loop Activities:
+
+// Find Maximum Value:
+// Here's a function that finds the maximum value in an array of integers using a foreach loop:
+public static int FindMaxValue(int[] numbers)
+{
+    int max = numbers[0]; // Assume the first element is the maximum
+    foreach (int num in numbers)
+    {
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+    return max;
+}
+
+//6. Average of Grades:
+// You can calculate the average of grades entered by the user using a foreach loop as follows:
+List<int> grades = new List<int>();
+// Populate the 'grades' list with user input
+// ...
+
+int sum = 0;
+foreach (int grade in grades)
+{
+    sum += grade;
+}
+double average = (double)sum / grades.Count;
+Console.WriteLine($"Average Grade: {average:F2}");
+
+// 7. Do-While Loop Activities:
+
+// User Menu with Do-While:
+// Here's a program that displays a menu to the user using a do-while loop:
+int choice;
+do
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Subtract");
+    Console.WriteLine("3. Quit");
+    Console.Write("Enter your choice: ");
+    choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("You selected Add.");
+            break;
+        case 2:
+            Console.WriteLine("You selected Subtract.");
+            break;
+        case 3:
+            Console.WriteLine("Exiting the program.");
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+} while (choice != 3);
+
+// 8. Factorial with Do-While:
+// You can calculate the factorial of a number n using a do-while loop as follows:
+public static long CalculateFactorialWithDoWhile(int n)
+{
+    long factorial = 1;
+    int i = 1;
+    do
+    {
+        factorial *= i;
+        i++;
+    } while (i <= n);
+    return factorial;
 }
