dc05ab7d2ef1f62fdc083a3a3332d61ace640e81
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-05 13:02:03 +0400

    try catch finally activities

--- Activities/exception handling/activities/activity.md
+++ Activities/exception handling/activities/activity.md
@@ -1,1 +1,31 @@
-1. Write a function called Divide that takes two integers as parameters and returns the result of dividing the first by the second. Handle exceptions like division by zero.
+Q0. Write a function called Divide that takes two integers as parameters and returns the result of dividing the first by the second. Handle exceptions like division by zero.
+
+Basic Try-Catch:
+Q1. Write a program that attempts to divide two numbers, numerator and denominator, inside a try block. Catch any DivideByZeroException that may occur and print an error message if it does.
+
+Multiple Catch Blocks:
+Q2. Modify the previous program to catch both DivideByZeroException and ArithmeticException. Print different error messages for each type of exception.
+
+Finally Block:
+Q3. Create a program that reads data from a file within a try block. Add a finally block to ensure that the file is always closed, regardless of whether an exception occurs or not.
+
+Custom Exception:
+Q4. Define a custom exception class, InvalidDataException, that inherits from Exception. Write a program that attempts to process some data and throws this custom exception if the data is invalid. Catch and handle the custom exception in the program.
+
+Nested Try-Catch Blocks:
+Q5. Write a program with nested try and catch blocks. Inside the inner try block, attempt to access an element in an array that is out of bounds. Catch the IndexOutOfRangeException in the inner catch block, and catch any other exceptions that may occur in the outer catch block.
+
+Throwing Exceptions:
+Q6. Create a program that reads a user's age and throws an exception if the age is negative or greater than 120. Catch the exception and print an error message.
+
+Using finally for Cleanup:
+Q7. Write a program that opens a database connection within a try block, performs some database operations, and closes the connection in a finally block to ensure proper cleanup, even if an exception occurs.
+
+Handling AggregateException:
+Q8. In a multithreaded application, use multiple threads to perform tasks concurrently. Catch and handle an AggregateException to manage exceptions that occur in parallel tasks.
+
+Rethrowing Exceptions:
+Q9. Write a program that calls a method that may throw an exception. Catch the exception and rethrow it with additional information using the throw statement.
+
+Using using Statement:
+Q10. Demonstrate the use of the using statement to automatically dispose of resources such as files or database connections. Create a program that reads data from a file using the StreamReader class within a using block.
--- Activities/exception handling/activities/answers.cs
+++ Activities/exception handling/activities/answers.cs
@@ -1,3 +1,4 @@
+// 0
 public double Divide(int dividend, int divisor)
 {
     try
@@ -10,3 +11,146 @@ public double Divide(int dividend, int divisor)
         return double.NaN; // Not-a-Number
     }
 }
+
+
+// Basic Try-Catch:
+// A1. You can write a program like this:
+
+try
+{
+    int numerator = 10;
+    int denominator = 0;
+    int result = numerator / denominator; // Attempt division by zero
+    Console.WriteLine("Result: " + result);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
+
+// Multiple Catch Blocks:
+// A2. You can modify the program to catch multiple exceptions:
+try
+{
+    int numerator = 10;
+    int denominator = 0;
+    int result = numerator / denominator; // Attempt division by zero
+    Console.WriteLine("Result: " + result);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("DivideByZeroException: " + ex.Message);
+}
+catch (ArithmeticException ex)
+{
+    Console.WriteLine("ArithmeticException: " + ex.Message);
+}
+
+// Finally Block:
+// A3. You can create a program like this:
+FileStream fileStream = null;
+try
+{
+    fileStream = new FileStream("data.txt", FileMode.Open);
+    // Read data from the file
+}
+catch (IOException ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
+finally
+{
+    if (fileStream != null)
+        fileStream.Close(); // Ensure the file is closed, even if an exception occurs
+}
+
+// Custom Exception:
+// A4. Define a custom exception class like this:
+public class InvalidDataException : Exception
+{
+    public InvalidDataException(string message) : base(message)
+    {
+    }
+}
+
+// Then, write a program that uses this custom exception:
+try
+{
+    // Process data and detect invalid data
+    throw new InvalidDataException("Invalid data detected.");
+}
+catch (InvalidDataException ex)
+{
+    Console.WriteLine("Custom Exception: " + ex.Message);
+}
+
+// Nested Try-Catch Blocks:
+// A5. You can create nested try-catch blocks like this:
+try
+{
+    try
+    {
+        int[] numbers = { 1, 2, 3 };
+        int value = numbers[10]; // Accessing an out-of-bounds element
+    }
+    catch (IndexOutOfRangeException ex)
+    {
+        Console.WriteLine("Inner Exception: " + ex.Message);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Outer Exception: " + ex.Message);
+}
+
+// Throwing Exceptions:
+// A6. Write a program that reads a user's age and throws an exception if it's negative or greater than 120:
+try
+{
+    int age = /* Input from user */;
+    if (age < 0 || age > 120)
+    {
+        throw new Exception("Invalid age.");
+    }
+    Console.WriteLine("Valid age: " + age);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
+
+// Using finally for Cleanup:
+// A7. Create a program that opens a database connection within a try block, performs some database operations, and closes the connection in a finally block:
+SqlConnection connection = null;
+try
+{
+    connection = new SqlConnection("connectionString");
+    connection.Open();
+    // Perform database operations
+}
+finally
+{
+    if (connection != null)
+    {
+        connection.Close();
+    }
+}
+
+// Handling AggregateException:
+// A8. In a multithreaded application, you can use Task.WhenAll to run multiple tasks concurrently and then use AggregateException to handle exceptions that may occur in parallel tasks.
+
+// Rethrowing Exceptions:
+// A9. Write a program that calls a method that may throw an exception and rethrow it with additional information:
+try
+{
+    DoSomething();
+}
+catch (Exception ex)
+{
+    throw new Exception("An error occurred while doing something.", ex);
+}
+
+// Using using Statement:
+// A10. You can use the using statement to read data from a file and automatically dispose of resources:
+using (StreamReader reader = new StreamReader("data.txt"))
+{ string line; while ((line = reader.ReadLine()) != null) { Console.WriteLine(line); } }
