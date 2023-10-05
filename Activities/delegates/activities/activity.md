798a264cca8fe422fadb1839c03b47c891949d03
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-05 14:47:39 +0400

    delegate activities

--- Activities/delegates/activities/activity.md
+++ Activities/delegates/activities/activity.md
@@ -1,1 +1,16 @@
-1. Create an event handler system where you have a publisher class and multiple subscriber classes. The publisher raises events, and subscribers respond to those events.
+0. Create an event handler system where you have a publisher class and multiple subscriber classes. The publisher raises events, and subscribers respond to those events.
+
+Delegate Definition:
+Q1. What is a delegate in C#? Provide a brief definition and explain its purpose.
+
+Delegate Declaration:
+Q2. How do you declare a delegate in C#? Provide an example of declaring a delegate with a signature that matches a method.
+
+Delegate Usage: 3. Delegate Instantiation:
+Q3. How do you instantiate a delegate in C#? Provide an example of instantiating a delegate and associating it with a method.
+
+Delegate Invocation:
+Q4. Explain how you can invoke (call) a delegate in C#. Provide an example of invoking a delegate.
+
+Multicast Delegates:
+Q5. What are multicast delegates, and how do they differ from singlecast delegates? Provide an example of using multicast delegates.
--- Activities/delegates/activities/answers.cs
+++ Activities/delegates/activities/answers.cs
@@ -1,3 +1,4 @@
+// 0
 public class Publisher
 {
     public event EventHandler MyEvent;
@@ -26,3 +27,33 @@ var publisher = new Publisher();
 var subscriber = new Subscriber();
 subscriber.Subscribe(publisher);
 publisher.RaiseEvent(); // This will trigger the event.
+
+
+// 1
+// Delegate Basics:
+
+// Delegate Definition:
+// A1. A delegate in C# is a type that represents references to methods with a specific signature. It allows you to treat methods as first-class objects, enabling you to pass methods as arguments to other methods, return methods from methods, and store methods in data structures. Delegates are often used for implementing event handling, callbacks, and creating extensible and flexible code.
+
+// Delegate Declaration:
+// A2. To declare a delegate in C#, you use the delegate keyword followed by the return type and parameters of the method signature that the delegate will represent. Here's an example:
+delegate int MathOperation(int x, int y);
+
+// 2
+// Delegate Usage:
+
+// Delegate Instantiation:
+// A3. To instantiate a delegate, you create an instance of the delegate type and associate it with a method that matches the delegate's signature. Here's an example:
+
+MathOperation add = (a, b) => a + b;
+
+// Delegate Invocation:
+// A4. You can invoke (call) a delegate by using its name followed by parentheses, passing the required arguments. Here's an example:
+int result = add(3, 5); // result will be 8
+
+// Multicast Delegates:
+// A5. Multicast delegates are delegates that can hold references to multiple methods. You can combine multiple delegates using the += operator and invoke them all with a single call. Here's an example:
+MathOperation add = (a, b) => a + b;
+MathOperation subtract = (a, b) => a - b;
+MathOperation multiDelegate = add + subtract;
+int result = multiDelegate(8, 4); // result will be 12 (add) and 4 (subtract)
